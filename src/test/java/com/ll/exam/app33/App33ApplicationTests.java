bbedfe1fdaf7ed7f01052737e3cf5249df91ab02
jhs512 <jangka512@gmail.com>
2022-09-08 21:59:28 +0900

    작업

--- src/test/java/com/ll/exam/app33/App33ApplicationTests.java
+++ src/test/java/com/ll/exam/app33/App33ApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles({"test", "dev"})
+@ActiveProfiles({"test"})
 class App33ApplicationTests {
 
     @Test

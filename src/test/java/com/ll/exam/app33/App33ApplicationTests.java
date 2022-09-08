03a00a86f501076e8bb876bf92618077fccc9707
jhs512 <jangka512@gmail.com>
2022-09-08 21:38:40 +0900

    작업

--- src/test/java/com/ll/exam/app33/App33ApplicationTests.java
+++ src/test/java/com/ll/exam/app33/App33ApplicationTests.java
@@ -2,12 +2,14 @@ package com.ll.exam.app33;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("test")
 class App33ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
